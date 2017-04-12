dd6e41a6228fdf228be6696a76b7fc693b731dca
Marcelo <marcelo.bohn@consyste.com.br>
2017-04-11 21:09:05 -0300

    inclusão da pasta app com estrutura do função contaVotos

---
+++ app/conta-votos.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function contaVotos(lista = [], opcoes = {}) {
+  // return {resultado: []};
+  // return {resultado: [{nome: 'João', votos: 2}, {nome: 'Carlos', votos: 1}, {nome: 'Pedro', votos: 3}]};
+}
+
+module.exports = { contaVotos };
