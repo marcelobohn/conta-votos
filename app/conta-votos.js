6e778b51698b3bfea41fef5625a1d136a4847b09
Marcelo <marcelo.bohn@consyste.com.br>
2017-04-18 20:42:56 -0300

    passando na segunda etapa da implementação #1

--- app/conta-votos.js
+++ app/conta-votos.js
@@ -2,7 +2,8 @@ function contaVotos(lista = [], opcoes = {}) {
   let r = { resultado: [] };
   lista.forEach(function(nome){
     // colocar no array de resultado
-    r.resultado.push({nome: nome, votos: 1});
+    if (!r.resultado.some(a => a.nome === nome))
+      r.resultado.push({nome: nome, votos: 1});
   });
 
   return r;
--- test/conta-votos.spec.js
+++ test/conta-votos.spec.js
@@ -15,17 +15,7 @@ lab.experiment('quando não informa parâmetros', () => {
 
 // primeira versão da implementação que apenas coloca os nomes no resultado
 lab.experiment('testes temporários', () => {
-  lab.test('retorna os votos formatados', (done) => {
-      const lista = ['João', 'Carlos', 'João', 'Pedro', 'Pedro', 'Pedro'];
-      const retorno = { resultado:[
-        {nome: 'João', votos: 1}, {nome: 'Carlos', votos: 1}, {nome: 'João', votos: 1},
-        {nome: 'Pedro', votos: 1}, {nome: 'Pedro', votos: 1}, {nome: 'Pedro', votos: 1},
-      ] };
-      expect(contaVotos(lista)).to.equal(retorno);
-      done();
-  });
-
-  lab.test.skip('retorna os votos formatados sem duplicidade', (done) => {
+  lab.test('retorna os votos formatados sem duplicidade', (done) => {
       const lista = ['João', 'Carlos', 'João', 'Pedro', 'Pedro', 'Pedro'];
       const retorno = { resultado:[
         {nome: 'João', votos: 1}, {nome: 'Carlos', votos: 1}, {nome: 'Pedro', votos: 1},
