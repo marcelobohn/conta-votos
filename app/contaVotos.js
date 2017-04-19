1397d7b1f81e622ee8efd5001b8efefca67ad43d
Marcelo <marcelo.bohn@consyste.com.br>
2017-04-19 10:30:23 -0300

    Correção na importação

--- app/contaVotos.js
+++ app/contaVotos.js
@@ -1,4 +1,4 @@
-const { montaAtributos } = require('../app/montaAtributos');
+const { montaAtributos } = require('./montaAtributos');
 
 const inicializaVotacao = function() {
   return { resultado: [] };
